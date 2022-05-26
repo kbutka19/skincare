e7ccdbc5f30011fe849e717c4a489f87890fe0b3
kbutka <kejsi.butka@ikubinfo.al>
2022-05-26 17:07:10 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCTu8nykDRRmqz1WvQ6-zxmF-s6o_8Gfn8',
+  authDomain: 'ng-skincare-6e924.firebaseapp.com',
+  databaseURL: 'https://ng-skincare-6e924-default-rtdb.firebaseio.com',
+  projectId: 'ng-skincare-6e924',
+  storageBucket: 'ng-skincare-6e924.appspot.com',
+  messagingSenderId: '904335991916',
+  appId: '1:904335991916:web:5471bd5dac86dff399a449',
+  measurementId: 'G-3QB3JF126T'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCTu8nykDRRmqz1WvQ6-zxmF-s6o_8Gfn8',
+    authDomain: 'ng-skincare-6e924.firebaseapp.com',
+    databaseURL: 'https://ng-skincare-6e924-default-rtdb.firebaseio.com',
+    projectId: 'ng-skincare-6e924',
+    storageBucket: 'ng-skincare-6e924.appspot.com',
+    messagingSenderId: '904335991916',
+    appId: '1:904335991916:web:5471bd5dac86dff399a449',
+    measurementId: 'G-3QB3JF126T'
+  
+  }
+};
